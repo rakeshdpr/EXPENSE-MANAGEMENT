dbc25af74dd7aa9b6b099bc82fe50bf7ad3f54d6
Rakesh Kumar <rakeshchauhan17dpr@gmail.com>
2024-07-02 20:16:22 +0530

    first commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4">
+      <h6 className="text-center text-white vh">
+        All rights reserved &copy;EXPENSE APP
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
